1cdca973fae82d775bc82e9b151c54aac8fd5e60
Lucas Silvestre Kloss Teles <klossteles@gmail.com>
2019-07-07 17:20:49 -0300

    p10: remove warning

--- pingpong.c
+++ pingpong.c
@@ -448,7 +448,8 @@ int sem_up (semaphore_t *s) {
 
     if (s->fila) {
         // TODO: FIX WARNING!
-        task_t * task = queue_remove((queue_t **)&(s->fila), (queue_t *)s->fila);
+        task_t * task = s->fila;
+        queue_remove((queue_t **)&(s->fila), (queue_t *)s->fila);
         task_resume(task);
     }
 
@@ -467,7 +468,8 @@ int sem_destroy (semaphore_t *s) {
 
     while (s->fila) {
         // TODO: FIX WARNING!
-        task_t * task = queue_remove((queue_t **)&(s->fila), (queue_t *)s->fila);
+        task_t * task = s->fila;
+        queue_remove((queue_t **)&(s->fila), (queue_t *)s->fila);
         task_resume(task);
     }
 
